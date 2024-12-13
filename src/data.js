3f2a101f0ce2afa91c0604f4bc0b7f60953171d2
Mohammad Sirath <noormohammadsirath5@gmail.com>
2024-12-12 18:19:59 -0800

    The courses routed by useParams successfully

--- src/data.js
+++ src/data.js
@@ -4,23 +4,27 @@ export let courseData = [
     title: "دوره ریکت",
     text: "آموزش پروژه محور ریکت و ریداکس از مقدماتی تا پیشرفته",
     img: "https://next1code.ir/wp-content/uploads/2022/12/react-cover-500x286.jpg",
+    teacher: "محمد حسینی",
   },
   {
     id: 2,
     title: "دوره بوت استرپ",
     text: "آموزش بوت استرپ 5.2 پروژه محور با تمرین های زیاد",
     img: "https://next1code.ir/wp-content/uploads/2022/10/bootstrap-500x286.png",
+    teacher: "نثار احمد رمزی",
   },
   {
     id: 3,
     title: "دوره جاوا اسکریپت",
     text: "آموزش صفر تا صد جاوا اسکریپت به صورت پروژه محور",
     img: "https://next1code.ir/wp-content/uploads/2021/08/couse-cover-500x286.jpg",
+    teacher: "عباس رحیمی",
   },
   {
     id: 4,
     title: "دوره طراحی ریسپانسیو",
     text: "آموزش طراحی وب ریسپانسیو با مدیا کوئری ها و فلکس باکس",
     img: "https://next1code.ir/wp-content/uploads/2021/06/rwd-couse-cover-500x286.jpg",
+    teacher: "علی رحیمی",
   },
 ];
